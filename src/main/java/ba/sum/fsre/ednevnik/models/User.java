07f0aa02ab46c01f324a392c60577a4a505f4201
mata94 <golemac14@gmail.com>
2023-11-28 11:10:46 +0100

    java test Ednevnik

---
+++ src/main/java/ba/sum/fsre/ednevnik/models/User.java
@@ -0,0 +1,62 @@
+package ba.sum.fsre.ednevnik.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+     String ime;
+     String prezime;
+     String email;
+
+    public User() {
+
+    }
+
+    public User(Long id, String ime, String prezime, String email) {
+        this.id = id;
+        this.ime = ime;
+        this.prezime = prezime;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+}
---
+++ src/test/java/ba/sum/fsre/ednevnik/EdnevnikApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.sum.fsre.ednevnik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdnevnikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
